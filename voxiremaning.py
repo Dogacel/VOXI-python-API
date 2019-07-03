0b3ec88a9dd338e97ff5ad7264e418a5fb012001
Dogacel <dogacel@gmail.com>
2019-07-03 10:11:30 +0100

    Added login file option.

--- voxiremaning.py
+++ voxiremaning.py
@@ -7,17 +7,26 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--username', help='Username for VOXI account', type=str)
 parser.add_argument('-p', '--password', help='Password for VOXI account', type=str)
+parser.add_argument('-l', '--loginfile', help='Login file in json forat with username and password', type=str)
 parser.add_argument('-d', '--debug', help='Dump the json returned in more detailed form', action='store_true')
+parser.add_argument('-j', '--json', help='Only return the json data gathered', action='store_true')
 args = parser.parse_args()
 
-if args.debug:
-	import pprint
+if args.debug or args.json:
+    import pprint
 
 
 signurl = 'https://www.voxi.co.uk/auth/sign-in'
+
+if args.loginfile:
+    infile = open(args.loginfile)
+    jsontmp = json.loads(''.join(infile.readlines()))
+    args.username = jsontmp['username']
+    args.password = jsontmp['password']
+
 payload = {
-	'username': args.username.upper(),
-	'password': base64.b64encode(args.password.encode()),
+    'username': args.username.upper(),
+    'password': base64.b64encode(args.password.encode()),
 }
 
 session_requests = requests.session()
@@ -25,17 +34,18 @@ session_requests.get(signurl)
 result = session_requests.post(signurl, data = payload, headers = dict(referer = signurl))
 
 if args.debug:
-	print('login: {}'.format(result))
-
+    print('login: {}'.format(result))        
 
 result = session_requests.post('https://www.voxi.co.uk/dashboard', headers = dict(referer = signurl))
 jresult = json.loads(result.text)
 
 if args.debug:
-	pprint.pprint(jresult['data']['currentPlan'])
+    pprint.pprint(jresult['data']['currentPlan'])
+elif args.json:
+    pprint.pprint(jresult)    
 else:
-	print('Data: {}/{}'.format(jresult['data']['currentPlan']['dataRemaining'],
-				   jresult['data']['currentPlan']['dataTotal']))
-	print('International Voice: {}/{}'.format(jresult['data']['extrasAndPasses'][0]['consumables'][0]['remaining'],
-						  jresult['data']['extrasAndPasses'][0]['consumables'][0]['total']))	
+    print('Data: {}/{}'.format(jresult['data']['currentPlan']['dataRemaining'],
+                               jresult['data']['currentPlan']['dataTotal']))
+    print('International Voice: {}/{}'.format(jresult['data']['extrasAndPasses'][0]['consumables'][0]['remaining'],
+                                              jresult['data']['extrasAndPasses'][0]['consumables'][0]['total']))	
 
